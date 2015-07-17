47c88e40ef4ce42f3fdf6616e36801d429e5cffe
FuryZhang <njuzhangxiao@gmail.com>
2015-07-17 15:59:49 +0800

    进一步简化“回文”问题的代码
    参考了他人的

--- algorithms/ValidPalindrome.cpp
+++ algorithms/ValidPalindrome.cpp
@@ -29,27 +29,19 @@ class Solution {
 public:
     bool isPalindrome(string s)
     {
-        string temp;
-        for (int i = 0; i < s.size(); ++i)
-        {
-            char a = s.at(i);
-            if (a >= 'A' && a <= 'Z')
-            {
-                temp.append(1, char(a + 'a' - 'A'));
-                continue;
-            }
-
-            if (a >= 'a' && a<= 'z' || a >= '0' && a <= '9')
-            {
-                temp.append(1, a);
-            }
-        }
-
         int i = 0;
-        int j = temp.size() - 1;
+        int j = s.size() - 1;
         while (i < j)
         {
-            if (temp.at(i) != temp.at(j))
+            while (i < j && !isalnum(s.at(i)))
+            {
+                i++;
+            }
+            while (i < j && !isalnum(s.at(j)))
+            {
+                j--;
+            }
+            if (tolower(s.at(i)) != tolower(s.at(j)))
             {
                 return false;
             }
