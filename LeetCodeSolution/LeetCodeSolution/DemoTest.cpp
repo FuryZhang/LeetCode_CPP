29ce8e960de07225486fa6a89b2e9b96d33e3627
FuryZhang <njuzhangxiao@gmail.com>
2015-07-17 13:44:52 +0800

    Two Sum III

--- LeetCodeSolution/LeetCodeSolution/DemoTest.cpp
+++ LeetCodeSolution/LeetCodeSolution/DemoTest.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include "TwoSum2.cpp"
+#include "TwoSum3.cpp"
 
 int main()
 {
-    int a[4] = {2, 7, 11, 15};
-    std::vector<int> vin(a, a + 4);
-    Solution solution;
-    std::vector<int> vout = solution.twoSum(vin, 9);
+    TwoSum ts3;
+    ts3.add(1);
+    ts3.add(3);
+    ts3.add(5);
+
+    bool test1 = ts3.find(4);
+    bool test2 = ts3.find(7);
+    
     return 0;
 }
---
+++ algorithms/TwoSum3.cpp
@@ -0,0 +1,53 @@
+#include <map>
+
+/************************************************************************************************
+* 
+* Question:
+* Design and implement a TwoSum class. It should support the following operations: add
+* and find.
+* add(input) 每 Add the number input to an internal data structure.
+* find(value) 每 Find if there exists any pair of numbers which sum is equal to the value.
+* For example,
+* add(1); add(3); add(5); find(4)->true; find(7)->false
+* 
+* Solution:
+* add 每 O(1) runtime, find 每 O(n) runtime, O(n) space 每 Store input in hash table:
+* A simpler approach is to store each input into a hash table. To find if a pair sum exists,
+* just iterate through the hash table in O(n) runtime. Make sure you are able to handle
+* duplicates correctly.
+* 
+************************************************************************************************/
+
+class TwoSum {
+public:
+    void add(int input)
+    {
+        int count = table_.find(input) == table_.end() ? 0 : table_[input];
+        table_[input] = count + 1;
+    }
+
+    bool find(int value)
+    {
+        for (auto it = table_.begin(); it != table_.end(); ++it)
+        {
+            int num1 = it->first;
+            int num2 = value - num1;
+            if (num1 == num2)
+            {
+                if (it->second >= 2)
+                {
+                    return true;
+                }
+            } 
+            else if (table_.find(num2) != table_.end())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+private:
+    std::map<int, int> table_;
+};
